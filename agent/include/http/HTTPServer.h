d1d4214cf16ed560909e8df7e94a19c1c4cf9424
NamLeMinh226059 <namleminh1524@gmail.com>
2026-07-29 17:04:15 +0700

    feat: implement listening socket

--- agent/include/http/HTTPServer.h
+++ agent/include/http/HTTPServer.h
@@ -4,11 +4,24 @@
 #include <thread>
 #include <atomic>
 
+#if defined(_WIN32)
+#include <winsock2.h>
+#include <ws2tcpip.h>
+using SocketHandle = SOCKET;
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+using SocketHandle = int;
+#endif
+
 namespace http {
 
 class HTTPServer {
 public:
     explicit HTTPServer(agent::Agent &agent);
+    explicit HTTPServer(agent::Agent &agent, unsigned short port);
     ~HTTPServer();
 
     void start();
@@ -18,6 +31,9 @@ private:
     agent::Agent &agent_;
     std::thread serverThread_;
     std::atomic<bool> running_{false};
+    unsigned short port_{8080};
+    SocketHandle listenSocket_;
+    bool winsockStarted_{false};
 };
 
 } // namespace http
--- agent/src/http/HTTPServer.cpp
+++ agent/src/http/HTTPServer.cpp
@@ -1,10 +1,27 @@
 #include "http/HTTPServer.h"
 #include <chrono>
+#include <cstring>
+#include <system_error>
 
 namespace http {
 
 HTTPServer::HTTPServer(agent::Agent &agent)
-    : agent_(agent) {}
+    : agent_(agent), port_(agent::AgentConfig().serverPort) {
+#if defined(_WIN32)
+    listenSocket_ = INVALID_SOCKET;
+#else
+    listenSocket_ = -1;
+#endif
+}
+
+HTTPServer::HTTPServer(agent::Agent &agent, unsigned short port)
+    : agent_(agent), port_(port) {
+#if defined(_WIN32)
+    listenSocket_ = INVALID_SOCKET;
+#else
+    listenSocket_ = -1;
+#endif
+}
 
 HTTPServer::~HTTPServer() {
     stop();
@@ -12,6 +29,67 @@ HTTPServer::~HTTPServer() {
 
 void HTTPServer::start() {
     if (running_.load()) return;
+
+#if defined(_WIN32)
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return;
+    }
+    winsockStarted_ = true;
+#endif
+
+    // create socket
+#if defined(_WIN32)
+    listenSocket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+#else
+    listenSocket_ = socket(AF_INET, SOCK_STREAM, 0);
+#endif
+    if (listenSocket_ == -1 || listenSocket_ == INVALID_SOCKET) {
+        return;
+    }
+
+    int opt = 1;
+#if defined(_WIN32)
+    setsockopt(listenSocket_, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt));
+#else
+    setsockopt(listenSocket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#endif
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port_);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+    if (bind(listenSocket_, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) != 0) {
+        // cleanup
+#if defined(_WIN32)
+        closesocket(listenSocket_);
+#else
+        close(listenSocket_);
+#endif
+        listenSocket_ = -1;
+#if defined(_WIN32)
+        if (winsockStarted_) WSACleanup();
+        winsockStarted_ = false;
+#endif
+        return;
+    }
+
+    if (listen(listenSocket_, SOMAXCONN) != 0) {
+        // cleanup
+#if defined(_WIN32)
+        closesocket(listenSocket_);
+#else
+        close(listenSocket_);
+#endif
+        listenSocket_ = -1;
+#if defined(_WIN32)
+        if (winsockStarted_) WSACleanup();
+        winsockStarted_ = false;
+#endif
+        return;
+    }
+
     running_.store(true);
     serverThread_ = std::thread([this]() {
         while (running_.load()) {
@@ -23,7 +101,24 @@ void HTTPServer::start() {
 void HTTPServer::stop() {
     if (!running_.load()) return;
     running_.store(false);
+
     if (serverThread_.joinable()) serverThread_.join();
+
+    if (listenSocket_ != -1 && listenSocket_ != INVALID_SOCKET) {
+#if defined(_WIN32)
+        closesocket(listenSocket_);
+#else
+        close(listenSocket_);
+#endif
+        listenSocket_ = -1;
+    }
+
+#if defined(_WIN32)
+    if (winsockStarted_) {
+        WSACleanup();
+        winsockStarted_ = false;
+    }
+#endif
 }
 
 } // namespace http
